8460ae3194d2af6ff2e6aede66874820fce7008e
Nilo Redini <n.redini@gmail.com>
2012-04-06 17:37:13 +0200

    Made average of informations during daytime, and removed some informations about nighttime

--- data/conform.rb
+++ data/conform.rb
@@ -15,6 +15,12 @@ IN_LIGHT  = 6
 ENERGY    = 7
 WORKDAY   = 8
 
+# time define
+HALF_HOUR = 30
+HOUR_OCLOCK = 0
+START_NIGHT_HOUR = 23
+END_NIGHT_HOUR = 4
+
 filepath = ARGV[0]
 
 inputHoliday = CSV.open( "inputHoliday.csv", "w")
@@ -23,18 +29,39 @@ targetHoliday = CSV.open( "targetHoliday.csv", "w")
 inputWork = CSV.open( "inputWork.csv", "w")
 targetWork = CSV.open( "targetWork.csv", "w")
 
-
+sOutLight = 0
+sInLight = 0
+sEnergy = 0
 CSV.foreach( filepath ) do |row|
+
+  #nights hours to jump
+  if ( row[HOUR].to_i > START_NIGHT_HOUR || row[HOUR].to_i < END_NIGHT_HOUR  ) 
+    next
+  end
+
+  # make averages
+  if row[MINUTE].to_i == HALF_HOUR  
+    sOutLight = row[OUT_LIGHT].to_i
+    sInLight = row[IN_LIGHT].to_i
+    sEnergy = row[ENERGY].to_i
+    next
+  end
+
+  if row[MINUTE].to_i == HOUR_OCLOCK
+    sOutLight = ( sOutLight + row[OUT_LIGHT].to_i ) / 2
+    sInLight  = ( sInLight + row[IN_LIGHT].to_i ) / 2
+    sEnergy = ( sEnergy + row[ENERGY].to_i ) / 2    
+  end
 
   day = row[WORKDAY]
 
-
   if day == "0" then
-    inputHoliday  << [ row[MONTH], row[DAY], row[HOUR], row[MINUTE], row[OUT_LIGHT] ]
-    targetHoliday << [ row[IN_LIGHT], row[ENERGY] ]
+    inputHoliday  << [ row[MONTH], row[DAY], row[HOUR], sOutLight.to_s ]
+    targetHoliday << [ sInLight.to_s, sEnergy.to_s ]
   else
-    inputWork  << [ row[MONTH], row[DAY], row[HOUR], row[MINUTE], row[OUT_LIGHT] ]
-    targetWork << [ row[IN_LIGHT], row[ENERGY] ]
+
+    inputWork  << [ row[MONTH], row[DAY], row[HOUR], sOutLight.to_s ]
+    targetWork << [ sInLight.to_s, sEnergy.to_s ]
   end
 
 end
