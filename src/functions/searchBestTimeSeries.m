e7fbdcfad6b5d2a526952249c7b2df9d0d35fb97
nebirhos <nebirhos@aol.com>
2012-04-17 23:55:57 +0200

    also check input-error correlation

--- src/functions/searchBestTimeSeries.m
+++ src/functions/searchBestTimeSeries.m
@@ -4,94 +4,101 @@
 %
 % Output: [network, training, outputs, errors]
 
-function [ network, training, outputs, errors ] = searchBestTimeSeries( inputs, targets )
-    GOAL_MSE = 4000;
-    GOAL_REGRESSION = 0.95;
-
-    HIDDEN_LAYER_SIZES = 20:20;
+function [ network, training, outputs, errors, inputs ] = searchBestTimeSeries( inputs, targets )
+    DELAYS = 2:8;
+    HIDDEN_LAYER_SIZES = 20:25;
 
     TRAIN_RATIO     = 0.90;
     VALUATION_RATIO = 0.05;
     TEST_RATIO      = 0.05;
 
-    RETRAIN_ATTEMPTS = 1;
-
-    bestMSE = inf;
-    bestReg = 0;
+    RETRAIN_ATTEMPTS = 10;
 
     % Prepare data
     inputSeries = tonndata( inputs, false, false );
     targetSeries = tonndata( targets, false, false );
 
     for i = HIDDEN_LAYER_SIZES
-        % Create a Nonlinear Autoregressive Network with External Input
-        inputDelays = 1:4;
-        feedbackDelays = 1:4;
-        hiddenLayerSize = 20;
-        net = narxnet( inputDelays, feedbackDelays, i );
+        for d = DELAYS
 
-        [inputs,inputStates,layerStates,targets] = preparets(net,inputSeries,{},targetSeries);
+            % Create a Nonlinear Autoregressive Network with External Input
+            inputDelays = 1:d;
+            feedbackDelays = 1:d;
+            hiddenLayerSize = i;
+            net = narxnet( inputDelays, feedbackDelays, hiddenLayerSize );
 
-        net.divideFcn = 'dividerand';  % Divide data randomly
-        net.divideMode = 'value';  % Divide up every value
-        net.divideParam.trainRatio = 70/100;
-        net.divideParam.valRatio = 15/100;
-        net.divideParam.testRatio = 15/100;
+            [inputs,inputStates,layerStates,targets] = preparets(net,inputSeries,{},targetSeries);
 
-        net.trainFcn = 'trainlm';  % Levenberg-Marquardt
-        net.performFcn = 'mse';  % Mean squared error
+            net.divideFcn = 'dividerand';  % Divide data randomly
+            net.divideMode = 'value';  % Divide up every value
+            net.divideParam.trainRatio = 70/100;
+            net.divideParam.valRatio = 15/100;
+            net.divideParam.testRatio = 15/100;
 
-        % No GUI
-        net.trainParam.showWindow = false;
-        net.trainParam.showCommandLine = false;
+            net.trainFcn = 'trainlm';  % Levenberg-Marquardt
+            net.performFcn = 'mse';  % Mean squared error
 
-        net.plotFcns = {'plotperform','plottrainstate','plotresponse', 'ploterrcorr', 'plotinerrcorr'};
+            % No GUI
+            net.trainParam.showWindow = true;
+            net.trainParam.showCommandLine = false;
 
-        for j = 1:RETRAIN_ATTEMPTS
-            disp( sprintf( 'HiddenLayerSize: %d - Attempt #%d', i, j) );
-            net = init( net );
+            net.plotFcns = {'plotperform','plottrainstate','plotresponse', 'ploterrcorr', 'plotinerrcorr'};
 
-            % Train the Network
-            [net,tr] = train(net,inputs,targets,inputStates,layerStates);
+            for j = 1:RETRAIN_ATTEMPTS
+                disp( sprintf( ['HiddenLayerSize: %d - Delay: 1:%d - Attempt #%d'], i, d, j) );
+                net = init( net );
 
-            % Test the Network
-            out = net(inputs,inputStates,layerStates);
-            err = gsubtract(targets,out);
-            mse = perform(net,targets,out)
+                % Train the Network
+                [net,tr] = train(net,inputs,targets,inputStates,layerStates);
 
-            % Recalculate Training, Validation and Test Performance
-            trainTargets = gmultiply(targets,tr.trainMask);
-            valTargets = gmultiply(targets,tr.valMask);
-            testTargets = gmultiply(targets,tr.testMask);
-            trainPerformance = perform(net,trainTargets,out)
-            valPerformance = perform(net,valTargets,out)
-            testPerformance = perform(net,testTargets,out)
+                % Test the Network
+                out = net(inputs,inputStates,layerStates);
+                err = gsubtract(targets,out);
+                mse = perform(net,targets,out)
 
-            network  = net;
-            errors   = err;
-            outputs  = out;
-            training = tr;
+                % Recalculate Training, Validation and Test Performance
+                trainTargets = gmultiply(targets,tr.trainMask);
+                valTargets = gmultiply(targets,tr.valMask);
+                testTargets = gmultiply(targets,tr.testMask);
+                trainPerformance = perform(net,trainTargets,out)
+                valPerformance = perform(net,valTargets,out)
+                testPerformance = perform(net,testTargets,out)
 
-            valid = checkErrcorr( errors )
-        end
-    end
+                network  = net;
+                errors   = err;
+                outputs  = out;
+                training = tr;
+
+                errcorr = checkCorr( errors, errors )
+                inecorr = checkCorr( inputs, errors )
+
+                if inecorr && errcorr
+                    disp( 'Best net found!' );
+                    return
+                end
+            end  % end ATTEMPTS for
+        end  % end DELAYS for
+    end  % end HIDDEN for
 
 end
 
 % Check error autocorrelation
-% Return true if autocorrelation not too bad
-function valid = checkErrcorr(e)
+% Return TRUE if correlation values aren't too bad
+function valid = checkCorr( a, b )
     STEPS = 20;
-    corr = nncorr( e, e, STEPS, 'unbiased' );
+    size = min( length(a), length(b) );
+    a = a(1,1:size);
+    b = b(1,1:size);
+    corr = nncorr( a, b, STEPS, 'unbiased' );
     corr = corr{1,1};
-    corr_zero = nncorr( e, e, 0, 'unbiased' );
+    corr_zero = nncorr( a, b, 0, 'unbiased' );
     corr_zero = corr_zero{1,1}
-    maxlagi = min( STEPS, numtimesteps(e) - 1 );
-    corr_limit = corr( maxlagi+1 )*2 / sqrt( length(e) );
-    
+    maxlagi = min( STEPS, numtimesteps(a) - 1 );
+    corr_limit = corr( maxlagi+1 )*2 / sqrt( length(a) );
+
     bad_steps = 0;
     for c = corr
-        if c > corr_limit*1.5
+        if abs(c) > abs(corr_limit*1.5)
             bad_steps = bad_steps + 1
         end
     end
