2987589ee1c54f001db0f31d66f3ba1625aa5bd6
nebirhos <nebirhos@aol.com>
2012-04-18 12:15:42 +0200

    netfit plots errhist also for test and validation

--- src/netfit.m
+++ src/netfit.m
@@ -7,7 +7,11 @@ load '../data/targetAll.csv';
 
 [ net, train, outputs, errors ] = searchBestFitting( inputAll', targetAll' );
 
+errorsTrain = errors(:, train.trainInd)
+errorsVal   = errors(:, train.valInd)
+errorsTest  = errors(:, train.testInd)
+
 view ( net )
 figure, plotperform( train )
 figure, plotregression( targetAll', outputs )
-figure, ploterrhist( errors )
+figure, ploterrhist( errorsTrain, 'train', errorsVal, 'validation', errorsTest, 'test' )
