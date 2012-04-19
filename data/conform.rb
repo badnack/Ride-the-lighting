817a2abcf4f924a8cf14a54e5247fb6dcf526128
Nilo Redini <n.redini@gmail.com>
2012-04-19 11:15:27 +0200

    Improved outliers management

--- data/conform.rb
+++ data/conform.rb
@@ -23,27 +23,28 @@ ENERGY    = 7
 WORKDAY   = 8
 
 # time defines
-HOUR_HALF = 30
-HOUR_OCLOCK = 0
 
-NIGHT_START        = 0
-EARL_MORNING_START = 4
-MORNING_START      = 7
-AFTERNOON_START    = 12
-EVENING_START      = 17
+NIGHT_START         = 0
+EARL_MORNING_START  = 4
+MORNING_START       = 7
+AFTERNOON_START     = 12
+EVENING_START       = 17
+
+HOUR_HALF           = 30
+HOUR_OCLOCK         = 0
 
 OUT_LIGHT_LOW       = 200
 OUT_LIGHT_MEDIUM    = 500
 OUT_LIGHT_HIGH      = 800
 OUT_LIGHT_VERY_HIGH = 1000
 
-ENERGY_LOW    = 125
-ENERGY_MEDIUM = 375
-ENERGY_HIGH   = 500
+ENERGY_LOW          = 125
+ENERGY_MEDIUM       = 375
+ENERGY_HIGH         = 500
 
-IN_LIGHT_LOW    = 375
-IN_LIGHT_MEDIUM = 975
-IN_LIGHT_HIGH   = 1300
+IN_LIGHT_LOW        = 375
+IN_LIGHT_MEDIUM     = 975
+IN_LIGHT_HIGH       = 1300
 
 
 
@@ -54,57 +55,61 @@ average_outlight = 0
 average_inlight = 0
 average_energy = 0
 
-CSV.foreach( datapath ) do |row|
+def isOutlier( row )
 
-
-  #first outliers
+  # outliers to remove:
   if row[WORKDAY].to_i == 1
 
     if ( AFTERNOON_START+1..EVENING_START).include? row[HOUR].to_i  and ( OUT_LIGHT_MEDIUM+1..OUT_LIGHT_HIGH ).include? row[OUT_LIGHT].to_i and row[ENERGY].to_i > ENERGY_MEDIUM
-      next
+      return true
     end
-    
+
     if ( AFTERNOON_START+1..EVENING_START).include? row[HOUR].to_i  and row[OUT_LIGHT].to_i > OUT_LIGHT_HIGH and row[ENERGY].to_i > ENERGY_MEDIUM
-      next
-    end    
+      return true
+    end
 
     if ( MORNING_START+1..AFTERNOON_START ).include? row[HOUR].to_i and ( OUT_LIGHT_MEDIUM+1..OUT_LIGHT_HIGH).include? row[OUT_LIGHT].to_i  and row[IN_LIGHT].to_i < IN_LIGHT_LOW
-      next
+      return true
     end
 
     if ( EARL_MORNING_START+1..MORNING_START ).include? row[HOUR].to_i and row[OUT_LIGHT].to_i <= OUT_LIGHT_LOW and ( IN_LIGHT_LOW+1..IN_LIGHT_MEDIUM).include? row[IN_LIGHT].to_i
-      next
+      return true
     end
 
     if row[HOUR].to_i > EVENING_START and row[OUT_LIGHT].to_i <= OUT_LIGHT_LOW and ( IN_LIGHT_LOW+1..IN_LIGHT_MEDIUM).include? row[IN_LIGHT].to_i
-      next
+      return true
     end
-
 
   end
 
   if row[WORKDAY].to_i == 0
 
-      if ( AFTERNOON_START+1..EVENING_START).include? row[HOUR].to_i  and ( OUT_LIGHT_LOW+1..OUT_LIGHT_MEDIUM ).include? row[OUT_LIGHT].to_i and ( ENERGY_LOW+1..ENERGY_MEDIUM ).include? row[ENERGY].to_i
-        next
-      end
+    if ( AFTERNOON_START+1..EVENING_START).include? row[HOUR].to_i  and ( OUT_LIGHT_LOW+1..OUT_LIGHT_MEDIUM ).include? row[OUT_LIGHT].to_i and ( ENERGY_LOW+1..ENERGY_MEDIUM ).include? row[ENERGY].to_i
+      return true
+    end
 
     if ( MORNING_START+1..AFTERNOON_START ).include? row[HOUR].to_i and row[OUT_LIGHT].to_i > OUT_LIGHT_HIGH and row[ENERGY].to_i > ENERGY_MEDIUM
-      next
+      return true
     end
 
     if row[HOUR].to_i > EVENING_START and row[OUT_LIGHT].to_i <= OUT_LIGHT_LOW  and ( IN_LIGHT_LOW+1..IN_LIGHT_MEDIUM).include? row[IN_LIGHT].to_i
-      next
+      return true
     end
-    
+
     if ( EARL_MORNING_START+1..MORNING_START ).include? row[HOUR].to_i and row[OUT_LIGHT].to_i <= OUT_LIGHT_LOW and ( IN_LIGHT_LOW+1..IN_LIGHT_MEDIUM).include? row[IN_LIGHT].to_i
-      next
+      return true
     end
 
   end
 
-  
+end # end def
 
+
+CSV.foreach( datapath ) do |row|
+
+    if isOutlier( row )
+      next
+    end
 
   # Hour averages
   if row[MINUTE].to_i == HOUR_HALF
@@ -118,9 +123,6 @@ CSV.foreach( datapath ) do |row|
     average_energy = ( average_energy + row[ENERGY].to_f ) / 2
   end
 
-  # if average_inlight > 1000
-  #   next
-  # end
 
   date = Date.new( row[YEAR].to_i, row[MONTH].to_i, row[DAY].to_i )
   weekday = date.cwday
