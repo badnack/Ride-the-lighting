8e55dbeb2de2cbbc80856fb7897473843daca792
Nilo Redini <n.redini@gmail.com>
2012-04-20 10:17:40 +0200

    Basic anfis script completed

--- src/anfis/functions/searchBestAnfis.m
+++ src/anfis/functions/searchBestAnfis.m
@@ -7,11 +7,11 @@
 function [ network, bestTrainErrors, bestTestErrors, bestCheckErrors ] = searchBestAnfis( trnData, chData, testData )
 
     EPOCHES_VALUES = [30:35];
-    ATTEMPTS = [1:8];
-    MIN_MS = [4 4];
-    MAX_MS = [8 8];
-
-    numMFs  = MIN_MS;
+    GOAL_MSE       = 50;    
+    MS_VALUES      = [3:7];
+    MS_INIT        = [ MS_VALUES(1), MS_VALUES(1) ];
+    
+    numMFs  = MS_INIT;
     mfType  = 'gbellmf';
 
     test_inputs = testData(:,1:2);
@@ -21,52 +21,59 @@ function [ network, bestTrainErrors, bestTestErrors, bestCheckErrors ] = searchB
     bestTestErrors  = inf;
     bestCheckErrors = inf;
     bestTestMse = inf;
-
     network = 0;
-
+    mse = inf;
+    
     silent_mode = zeros( 4, 1 );
 
-    % tries the same epoch ATTEMPTS times
+    % tries the same differents epoches
     for epoch_n = EPOCHES_VALUES
-        numMFs  = MIN_MS;
-        
-        for i = ATTEMPTS
-            disp(sprintf( 'Epoches #%d - Attempt #%d', epoch_n, i + 1  ) );
 
-            % generates a fis with 5 gaussian bell membership functions for each input
-            in_fis = genfis1( trnData, numMFs, mfType );
-            
-            %anfis
-            [ out_fis, error, ss, chkFis, chkErr ] = anfis( trnData, in_fis, epoch_n, silent_mode, chData, 1 );
+        numMFs  = MS_INIT;
 
-            % tests the network
-            anfis_output = evalfis( test_inputs, out_fis );
+        % tries each values between Ms min value and ms max value
+        for i = MS_VALUES
+            for j = MS_VALUES
 
-            %FIXME: use mse instead of normal error
-            errTest = gsubtract( test_targets, anfis_output );
-
-            mse = sqrt( sum( (test_targets(:)-anfis_output(:)).^2) / numel(test_targets) );
-
-
-
-            %mse ( doesn't exist a matlab function)
-            if mse < bestTestMse
-                disp( 'best value found!' );
-                bestTestMse = mse;
-                bestTestErrors = errTest;
-                bestTrainErrors = error;
-                bestCheckErrors = chkErr;
+                numMFs  = [ i, j ];                
+                
+                % generates a fis with 5 gaussian bell membership functions for each input
+                in_fis = genfis1( trnData, numMFs, mfType );
+                
+                %anfis
+                [ out_fis, error, ss, chkFis, chkErr ] = anfis( trnData, in_fis, epoch_n, silent_mode, chData, 1 );
+                
+                % tests the network
+                anfis_output = evalfis( test_inputs, out_fis );
+                
+                %FIXME: use mse instead of normal error
+                errTest = gsubtract( test_targets, anfis_output );
+                
+                mse = sqrt( sum( (test_targets(:)-anfis_output(:)).^2) / numel(test_targets) );
+                
+                disp(sprintf( ['Epoches #%d - Mfs: [%d %d] - MSE #%d'], epoch_n, i, j, mse ) );
+                
+                
+                %mse ( doesn't exist a matlab function)
+                if mse < bestTestMse
+                    
+                    disp( 'best value found!' );
+                    bestTestMse = mse;
+                    bestTestErrors = errTest;
+                    bestTrainErrors = error;
+                    bestCheckErrors = chkErr;
+                    
+                    if mse <= GOAL_MSE
+                        return
+                    end
+                end
+                
+                
             end
             
-            mse
-            
-            %if i < 5 
-            %   numMFs(1) = numMFs(1) + 1;
-            %else numMFs(2) = numMFs(2) + 1;
-            %end
-
         end
-
     end
+
+    bestTestMse
 
 end
